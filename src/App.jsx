9a5f28b6e8e48a11f1ec64f3a5f7fcc138dcec5a
tuhin <tuhin.4mondal@gmail.com>
2025-10-07 01:03:31 -0700

    first (and probably only) commit

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
+
+export default function App() {
+  const [mainCount, setMainCount] = useState(0);
+
+  const [miniCounters, setMiniCounters] = useState([]);
+
+  function updateCounter() {
+    setMainCount((prevCount) => prevCount + 1);
+  }
+
+  const miniCounterElements = miniCounters.map((counter) => (
+    <button
+      className="mini-counter"
+      key={counter.id}
+      style={{
+        position: "absolute",
+        left: counter.x,
+        top: counter.y,
+        zIndex: counter.z,
+      }}
+      onClick={() => updateMiniCounters(counter.id)}
+    >
+      {counter.num}
+    </button>
+  ));
+
+  function addMiniCounter(event) {
+    if (
+      !["main-counter", "mini-counter", "click-button"].includes(
+        event.target.className
+      )
+    ) {
+      setMiniCounters((prevCounters) => [
+        ...prevCounters,
+        {
+          position: "absolute",
+          x: event.clientX - 11,
+          y: event.clientY - 25,
+          z: 2,
+          num: 1,
+          id: nanoid(),
+        },
+      ]);
+    }
+  }
+
+  function updateMiniCounters(id) {
+    console.log(miniCounters.length + 1);
+    setMiniCounters((prevCounters) =>
+      prevCounters.map((counter) =>
+        counter.id === id
+          ? { ...counter, num: counter.num + 1, z: 2 }
+          : { ...counter, z: 1 }
+      )
+    );
+  }
+
+  return (
+    <main onClick={addMiniCounter}>
+      {miniCounterElements}
+      <h1>You've clicked</h1>
+      <button onClick={updateCounter} className="main-counter">
+        {mainCount}
+      </button>
+      <h2>times!</h2>
+      <button onClick={updateCounter} className="click-button">
+        Click
+      </button>
+      <p>Pssst... try clicking somewhere random</p>
+    </main>
+  );
+}
